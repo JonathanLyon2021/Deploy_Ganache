da5f641239f5981eb86bb4702a15a819e0ef13fc
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-08-30 06:33:44 -0400

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -4,7 +4,13 @@ pragma solidity ^0.5.9;
 contract Counter{
         uint private count = 0;
 
-     function incrementorCounter() public {
-      count += 1;
+    function incrementorCounter() public {
+            count += 1;
+        }
+        
+    function decrementCounter() public {
+        count -= 1;
     }
 }
+
+
